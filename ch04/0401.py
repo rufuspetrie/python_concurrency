352afd6a9d05d505b0e2c854a36fee343acc54f7
Rufus <36833913+rufuspetrie@users.noreply.github.com>
2024-02-14 16:45:43 -0500

    chapter4

--- ch04/0401.py
+++ ch04/0401.py
@@ -1,17 +1,17 @@
-n_files = 254
+n_files = 10
 files = []
 
-# Method 1
+# Method 1 - this can fail because it doesn't close files (253 is maximum allowed)
 for i in range(n_files):
     files.append(open(f"output1/sample{i}.txt", "w"))
 
-# Method 2
+# Method 2 - manually close files
 for i in range(n_files):
     f = open(f"output1/sample{i}.txt", "w")
     files.append(f)
     f.close()
 
-# Method 3
+# Method 3 - the with context manager automatically closes files when finished
 for i in range(n_files):
     with open(f"output1/sample{i}.txt", "w") as f:
         files.append(f)
--- ch04/0402.py
+++ ch04/0402.py
@@ -2,14 +2,15 @@ from threading import Lock
 
 my_lock = Lock()
 
-# Induces deadlocks
+# Induces deadlocks - desired file does not exist so the lock never releases
 def get_data_from_file_v1(filename):
     my_lock.acquire()
     with open(filename, "r") as f:
         data.append(f.read())
     my_lock.release()
 
-# Handles exceptions
+# Handles exceptions - uses with context manager and includes a lock
+# This will automatically release the lock once we exit the context manager
 def get_data_from_file_v2(filename):
     with my_lock, open(filename, "r") as f:
         data.append(f.read())
