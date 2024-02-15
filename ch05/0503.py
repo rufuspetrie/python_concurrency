0dc243f15e57a2741dfef446aa02a9ef9bb2321b
rufuspetrie <36833913+rufuspetrie@users.noreply.github.com>
2024-02-14 23:13:27 -0500

    chapter5

---
+++ ch05/0503.py
@@ -0,0 +1,31 @@
+import threading
+import requests
+import time
+
+def ping(url):
+    res = requests.get(url)
+    print(f"{url}: {res.text}")
+
+urls = [
+    'http://httpstat.us/200',
+    'http://httpstat.us/400',
+    'http://httpstat.us/404',
+    'http://httpstat.us/408',
+    'http://httpstat.us/500',
+    'http://httpstat.us/524'
+]
+
+t0 = time.time()
+for url in urls:
+    ping(url)
+print(f"Sequential: {time.time() - t0:.2f} seconds\n")
+
+t0 = time.time()
+threads = []
+for url in urls:
+    thread = threading.Thread(target = ping, args = (url, ))
+    threads.append(thread)
+    thread.start()
+for thread in threads:
+    thread.join()
+print(f"Threaded: {time.time() - t0:.2f} seconds\n")
