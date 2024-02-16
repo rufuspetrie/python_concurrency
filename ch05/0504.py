4363d7988aa8a23f5e7b6209d03cf285e5255869
Rufus <36833913+rufuspetrie@users.noreply.github.com>
2024-02-15 21:36:02 -0500

    chapter5

---
+++ ch05/0504.py
@@ -0,0 +1,30 @@
+import threading
+import requests
+import time
+
+class MyThread(threading.Thread):
+    def __init__(self, url):
+        threading.Thread.__init__(self)
+        self.url = url
+        self.result = None
+
+    def run(self):
+        res = requests.get(self.url)
+        self.result = f"{self.url}: {res.text}"
+
+urls = [
+    'http://httpstat.us/200',
+    'http://httpstat.us/400',
+    'http://httpstat.us/404',
+    'http://httpstat.us/408',
+    'http://httpstat.us/500',
+    'http://httpstat.us/524'
+]
+
+t0 = time.time()
+threads = [MyThread(url) for url in urls]
+for thread in threads: thread.start()
+for thread in threads: thread.join()
+for thread in threads: print(thread.result)
+print(f"Took {time.time() - t0:.2f} seconds")
+print("Done")
