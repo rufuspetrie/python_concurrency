4cc2c4a103c431e78b75919072c8bb018595c84e
Rufus <36833913+rufuspetrie@users.noreply.github.com>
2024-02-18 20:34:09 -0500

    chapter8

---
+++ ch08/0801.py
@@ -0,0 +1,11 @@
+import cv2
+
+im = cv2.imread("input.ship.jpg")
+cv2.imshow("Test", im)
+cv2.waitKey(0)
+
+print(im)
+print("Type: ", type(im))
+print("Shape: ", im.shape)
+print("Top left pixel: ", im[0,0])
+print("Done")
